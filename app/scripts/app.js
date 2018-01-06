f4022a07c64f25fb412d04106c7011fd7cff2f03
Timothy Yuan <Timothy Yuan>
2018-01-06 13:44:21 -1000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webApp
+ * @description
+ * # webApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webApp', [
+    'ngSanitize'
+  ]);
