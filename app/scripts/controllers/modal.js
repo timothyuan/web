f8c0979ea02c443f10089633c582d4307c29525e
Timothy Yuan <Timothy Yuan>
2018-01-06 15:45:16 -1000

    videogular themes update

--- app/scripts/controllers/modal.js
+++ app/scripts/controllers/modal.js
@@ -12,7 +12,6 @@ app.controller('ModalCtrl', function($sce, $scope, close, Id) {
     sources: [
     {src: $sce.trustAsResourceUrl('res/videos/'+Id+'.mp4'), type: 'video/mp4'}
     ],
-    theme: 'bower_components/videogular-themes-default/videogular.css',
     plugins: {
       poster: 'res/thumbnails/'+Id+'.jpg'
     }
